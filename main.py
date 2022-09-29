d8e5617bc934b7c8e118e39fbc3ff11b0f8d520e
Mitch Hearn <mitchhearn@Mitchs-MacBook-Pro.local>
2022-09-29 12:48:03 +0100

    robot system failure

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import time
+
+print("System Failure Imminent!")
+time.sleep(2)
+print("System reboot has been initiated...")
+print()
+print()
+time.sleep(1)
+print("...rebooting sensory system")
+time.sleep(0.5)
+print("...rebooting output motors")
+time.sleep(0.5)
+print("...rebooting hover engine")
+time.sleep(1)
+print()
+print()
+print("System Online!")
